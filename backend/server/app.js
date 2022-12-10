fdd71df18b85857e6a89a8f814119a473d6dfd58
prajjwal2411 <prajjwal1997@gmail.com>
2022-12-10 12:44:47 +0530

    mean-application-day-2-api-routes-working-fixed

--- backend/server/app.js
+++ backend/server/app.js
@@ -6,23 +6,20 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const employeeRoutes = require('../routes/employee');
+
 mongoose.set('strictQuery', true)
 //Connecting with mongo db
-mongoose.connect(`https://ap-south-1.aws.data.mongodb-api.com/app/application-0-wtfzs/endpoint/mean_crud_app`).then((x) => {
+mongoose.connect(`mongodb+srv://prajjwal:prajjwal@projects-applications.plmb7cy.mongodb.net/?retryWrites=true&w=majority`).then((x) => {
     console.log(`Connected to Mongo! Database name: ${x.connections[0].name}`);
 }).catch((err) => {
     console.error(`Error connecting to mongo: ${err}`);
 })
 
-const employeeRoutes = require('../routes/employees');
-
-app.use(bodyParser.json());
-app.use(cors);
-
 const port = process.env.PORT || 4000;
 
-// Routes
-//app.use('/employees', employeeRoutes);
+//Routes
+app.use('/employees', employeeRoutes);
 
 const server = app.listen(port, function() {
     console.log(`Server listening on port: ${port}`);
